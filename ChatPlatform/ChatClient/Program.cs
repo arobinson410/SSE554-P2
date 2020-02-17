a73ce809d6151650a1bb6ddf2460318b5638f752
arobinson410 <arobinson410@gmail.com>
2020-02-17 16:13:19 -0500

    Add files via upload

---
+++ ChatPlatform/ChatClient/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace ChatClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Int32 port = 13000;
+            String server = "127.0.0.1";
+
+            Thread.Sleep(5000);
+
+            TcpClient client = new TcpClient(server, port);
+
+            Byte[] data = System.Text.Encoding.ASCII.GetBytes("Client Sending Message");
+
+            NetworkStream stream = client.GetStream();
+
+            stream.Write(data, 0, data.Length);
+
+            stream.Close();
+            client.Close();
+        }
+    }
+}
---
+++ ChatPlatform/ChatPlatform/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ChatPlatform
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Int32 port = 13000;
+            IPAddress localAddr = IPAddress.Parse("127.0.0.1");
+
+            TcpListener server = new TcpListener(localAddr, port);
+
+            server.Start();
+            Byte[] bytes = new byte[256];
+            String data = null;
+
+            TcpClient client = server.AcceptTcpClient();
+
+            NetworkStream stream = client.GetStream();
+
+            int i;
+            while((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+            {
+                data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
+                Console.WriteLine("Recieved: {0}", data);
+            }
+        }
+    }
+}
